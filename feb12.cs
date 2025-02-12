adca1da6dceb9fef90ecbd4c48817b41a3ce76aa
Lara682 <laraa448@stemcollegiate.ca>
2025-02-12 09:22:44 -0700

    Create feb12.cs

---
+++ feb12.cs
@@ -0,0 +1,8 @@
+Console.WriteLine('b');
+Console.WriteLine("Hello World");
+Console.WriteLine(123);
+Console.WriteLine(2.625);
+Console.WriteLine(0.25F);
+Console.WriteLine(12.3981M);
+Console.WriteLine(true);
+Console.WriteLine(false);
